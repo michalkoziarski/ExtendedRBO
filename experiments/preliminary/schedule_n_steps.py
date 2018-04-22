d2b96953c7fec04c4cedba2e606e26dbfd361dbb
michalkoziarski <michalkoziarski@gmail.com>
2018-04-22 18:29:39 +0200

    using linear scaling for n_steps analysis

--- experiments/preliminary/schedule_n_steps.py
+++ experiments/preliminary/schedule_n_steps.py
@@ -18,13 +18,14 @@ logging.info('Scheduling experiments...')
 for dataset in tqdm(datasets.names('preliminary')):
     for fold in range(1, 11):
         for classifier in ['KNN', 'CART', 'SVM', 'NB']:
-            for n_steps in [16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192]:
+            for n_steps in [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024]:
                 trial = {
                     'Algorithm': 'RBO+',
                     'Parameters': {
                         'gamma': 0.05,
                         'step_size': 0.001,
-                        'n_steps': n_steps
+                        'n_steps': n_steps,
+                        'n_steps_scaling': 'linear'
                     },
                     'Classifier': classifier,
                     'Dataset': dataset,
