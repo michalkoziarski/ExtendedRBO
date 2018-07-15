d4a4678aeb7d9a5d33a402a3373462106882ddc9
michalkoziarski <michalkoziarski@gmail.com>
2018-07-15 16:36:35 +0200

    scheduler for borderline

---
+++ experiments/preliminary/schedule_borderline.py
@@ -0,0 +1,39 @@
+import logging
+import os
+import sys
+
+from tqdm import tqdm
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+import databases
+import datasets
+
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info('Scheduling experiments...')
+
+
+for dataset in tqdm(datasets.names('preliminary')):
+    for fold in range(1, 11):
+        for classifier in ['KNN', 'CART', 'SVM', 'NB']:
+            for borderline in [False, True]:
+                trial = {
+                    'Algorithm': 'RBO+CV',
+                    'Parameters': {
+                        'gammas': [0.05, 0.5, 5.0],
+                        'step_size': 0.001,
+                        'n_steps': [1, 4, 16, 64, 256],
+                        'approximate_potential': True,
+                        'n_nearest_neighbors': 25,
+                        'borderline': borderline,
+                        'n_steps_scaling': 'linear'
+                    },
+                    'Classifier': classifier,
+                    'Dataset': dataset,
+                    'Fold': fold,
+                    'Description': 'Preliminary (borderline)'
+                }
+
+                databases.add_to_pending(trial)
